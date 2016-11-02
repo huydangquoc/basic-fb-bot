da00559556de44d20aeeab22077a74e34fc5f63d
Dang Quoc Huy <huysamdua@yahoo.com>
2016-11-02 15:30:17 +0700

    Setup simple post action to receive message from Facebook

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,9 +1,9 @@
 class BotController < ApplicationController
   def webhook
-    if params['hub.verify_token'] == "mytoken"
-      render text: params['hub.challenge'] and return
+    if params['hub.verify_token'] == "huydang_nicachenchalaca"
+      render plain: params['hub.challenge'] and return
     else
-      render text: 'error' and return
+      render plain: 'error' and return
     end
   end
 
@@ -12,5 +12,5 @@ class BotController < ApplicationController
   end
 end
 
-# Token
-# EAARSROQbSfIBAHOSqsozPtelFZB7I30jzKMzThbVtZBBa4sCNQzapxebmCDXzwIZC3iZCtgLZBLhKAUGZC1Scsvof56xX0svI0mjpv8d02Ehya3WirRBSqrzMVAqj3fDUIlFncSSUJQuwilavXSnooVCoPm0dARc69kMOGZAqh6rgZDZD
+# Page Access Token
+# EAAWZCXBOxp7gBAD1yQO6NAHRgGWEwMDjXSOx3eubUqFsY6uDtUiz1f7cAgje7QTqbJ8tCq1VzVjGHVUpqkxqSsJs0DurCVqsZCYxkGTTXavGOOdc3CRkZBY4blwsI4cTtCjCoRrPYYbj9094E3YFWEtYiiZC582OqrbC3BebmQZDZD
